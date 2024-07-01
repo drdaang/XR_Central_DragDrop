e4f436450474d8047540a81bef07276c7a5b147c
drdaang <99331378+drdaang@users.noreply.github.com>
2024-07-01 10:44:06 +0530

    added a object list instead of normal list for indexes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
   return (
     <div>
       <DndProvider backend={HTML5Backend}>
-        <Component ans={["speak", "very well","I","can"]} correct={0} ques={"THIS IS THE GIVEN QUESTION?"} />
+        <Component ans={[{ id: 0, name: 'Option 1' },
+    { id: 1, name: 'Option 2' },
+    { id: 2, name: 'Option 3' },
+    { id: 3, name: 'Option 4' },]} correct={0} ques={"THIS IS THE GIVEN QUESTION?"} />
        
       </DndProvider>
     </div>
